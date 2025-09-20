e6dfea301b7116ecaea42bdec5f07539c1ee5317
serennia <yihookan@yahoo.co.jp>
2025-09-21 01:10:07 +0900

    Adds formatted paste commands
    Adds commands to paste clipboard content as a header, inline code, or code block.
    
    Improves user experience by providing formatting options when pasting content.
    Allows users to select a language for code blocks via a modal.
    
    Adds a settings tab for plugin configuration.

--- main.ts
+++ main.ts
@@ -1,81 +1,108 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface FormattedPasterPluginSettings {
+	code_language: Record<string, string>;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: FormattedPasterPluginSettings = {
+	code_language: {
+		'': 'Plain Text',
+		'csharp': 'C#',
+		'javascript': 'JavaScript',
+		'python': 'Python',
+		'java': 'Java',
+		'ruby': 'Ruby',
+		'go': 'Go',
+		'php': 'PHP',
+		'swift': 'Swift',
+		'kotlin': 'Kotlin',
+		'rust': 'Rust',
+		'typescript': 'TypeScript',
+		'json': 'JSON',
+		'html': 'HTML',
+		'css': 'CSS',
+		'bash': 'Bash',
+		'yaml': 'YAML',
+	}
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class FormattedPasterPlugin extends Plugin {
+	settings: FormattedPasterPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (_evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		function insertAtCursor(editor: Editor, text: string) {
+			const cursor = editor.getCursor();
+			editor.replaceRange(text, cursor);
+			editor.setCursor({ line: cursor.line, ch: cursor.ch + text.length });
+		}
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// paste as a Header command
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+			id: 'paste-as-header',
+			name: 'Paste As Header',
+			editorCallback: async (editor: Editor, _view: MarkdownView) => {
+
+				const clipboardText = await navigator.clipboard.readText();
+				if (!clipboardText) {
+					new Notice('Clipboard is empty!');
+					return;
+				}
+
+				const headerText = `# ${clipboardText.trim()}\n`;
+				console.log('Pasting header: ' + headerText);
+				insertAtCursor(editor, headerText);
+				new Notice(`Pasted as header! ${headerText}`);
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
+
+		// paste as inline code command
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, _view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+			id: 'paste-as-inline-code',
+			name: 'Paste As Inline Code',
+			editorCallback: async (editor: Editor, _view: MarkdownView) => {
+
+				const clipboardText = await navigator.clipboard.readText();
+				if (!clipboardText) {
+					new Notice('Clipboard is empty!');
+					return;
+				}
+
+				const inlineCode = `\`${clipboardText.trim()}\``;
+				console.log('Pasting inline code: ' + inlineCode);
+				insertAtCursor(editor, inlineCode);
+				new Notice(`Pasted as inline code! ${inlineCode}`);
 			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
+		})
+
+		// paste as code block command
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+			id: 'paste-as-code-block',
+			name: 'Paste As Code Block',
+			editorCallback: async (editor: Editor, _view: MarkdownView) => {
+
+				const clipboardText = await navigator.clipboard.readText();
+				if (!clipboardText) {
+					new Notice('Clipboard is empty!');
+					return;
+				}
+
+				new ChosenLanguageModal(this.app, this.settings.code_language, (language) => {
+					if (language === undefined) {
+						return;
 					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+					const codeBlock = `\`\`\`${language}\n${clipboardText}\n\`\`\``;
+					insertAtCursor(editor, codeBlock);
+					new Notice('Pasted code block!');
+				}).open();
 			}
 		});
 
+
+
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new FormattedPasterPluginSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -84,6 +111,7 @@ export default class MyPlugin extends Plugin {
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		console.log('Loaded settings:', this.settings);
 	}
 
 	async saveSettings() {
@@ -91,44 +119,75 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+export class ChosenLanguageModal extends Modal {
+	private onChoose?: (language?: string) => void;
+	private code_language: Record<string, string>;
+	constructor(app: App, code_language: Record<string, string>, onChoose?: (language?: string) => void) {
 		super(app);
+		this.onChoose = onChoose;
+		this.code_language = code_language;
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+
+		contentEl.addEventListener('keydown', (event) => {
+			if (event.key === 'Escape') {
+				this.onChoose?.(undefined);
+				this.close();
+			}
+
+			if (event.key === 'Enter') {
+				const dropdown = contentEl.querySelector('select');
+				if (dropdown) {
+					const value = (dropdown as HTMLSelectElement).value;
+					console.log('Chosen language: ' + value);
+					this.onChoose?.(value);
+					this.close();
+				}
+			}
+		})
+
+		new Setting(contentEl)
+			.setName('Choose Language')
+			.setDesc('Select a programming language for the code block')
+			.addDropdown(dropdown => {
+				dropdown.addOptions(this.code_language)
+					.setValue('csharp')
+					.onChange((value) => {
+						console.log('Chosen language: ' + value);
+						this.onChoose?.(value);
+						this.close();
+					})
+			})
 	}
 
 	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
+class FormattedPasterPluginSettingTab extends PluginSettingTab {
+	plugin: FormattedPasterPlugin;
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: FormattedPasterPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
-
+		const { containerEl } = this;
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName('Code Block Paster Setting')
+			.setDesc('This is a setting for the Code Block Paster plugin')
+			.addButton(button => {
+				button.setButtonText('Click me')
+					.onClick(async () => {
+						const json = JSON.stringify(this.plugin.settings.code_language, null, 2);
+						console.log(json);
+						new Notice(json);
+						await this.plugin.saveSettings();
+					});
+			});
 	}
 }
